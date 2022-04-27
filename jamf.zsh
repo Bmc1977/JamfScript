577f235931d0c8d39f12e9a228ffcdde1bd1fed9
Braxton Lamey <braxlamey@gmail.com>
2022-04-27 16:22:12 -0400

    final?

--- jamf.zsh
+++ jamf.zsh
@@ -1,7 +1,9 @@
 jamf -removeFramework
 cd /var/db/
 mv ConfigurationProfiles ConfigurationProfilesOLD
+mkdir ConfigurationProfiles
 cd ConfigurationProfiles
 mkdir Settings
 touch Settings/.profilesAreInstalled
 profiles renew -type enrollment
+jamf policy
